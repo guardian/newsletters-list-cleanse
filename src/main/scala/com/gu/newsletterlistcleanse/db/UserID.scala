c31c0d302804258ac14b9e57dd81bf0cc9562c0f
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2020-09-14 10:03:04 +0100

    Remove Athena support

--- src/main/scala/com/gu/newsletterlistcleanse/db/UserID.scala
+++ src/main/scala/com/gu/newsletterlistcleanse/db/UserID.scala
@@ -1,13 +1,5 @@
 package com.gu.newsletterlistcleanse.db
 
-import scalikejdbc.WrappedResultSet
-
 case class UserID(
   userId: String
 )
-
-object UserID {
-
-  def fromRow(rs: WrappedResultSet): UserID =
-    UserID(userId = rs.string("user_id"))
-}
