753ef6884c73ce2ca7d54571cb34412131dc70fd
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2021-01-06 11:31:05 +0000

    Remove traces of access to the datalake via Athena

--- src/main/scala/com/gu/newsletterlistcleanse/NewsletterAWSCredentialProvider.scala
+++ src/main/scala/com/gu/newsletterlistcleanse/NewsletterAWSCredentialProvider.scala
@@ -3,10 +3,6 @@ package com.gu.newsletterlistcleanse
 import com.amazonaws.auth.{AWSCredentialsProviderChain, DefaultAWSCredentialsProviderChain}
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 
-class NewsletterAWSCredentialProvider extends AWSCredentialsProviderChain(
-  new ProfileCredentialsProvider("ophan"),
-  DefaultAWSCredentialsProviderChain.getInstance())
-
 class NewsletterSQSAWSCredentialProvider extends AWSCredentialsProviderChain(
   new ProfileCredentialsProvider("identity"),
   DefaultAWSCredentialsProviderChain.getInstance())
