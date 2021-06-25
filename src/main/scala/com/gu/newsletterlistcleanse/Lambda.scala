63ec22dd624bdf059a6ba00b72693716014be2a9
Yusuf Faraji <yusuf.faraji@gmail.com>
2021-06-25 16:14:26 +0100

    Add AwsCredentialProviderV2 as required by AWS  SDK changes

--- src/main/scala/com/gu/newsletterlistcleanse/Lambda.scala
+++ src/main/scala/com/gu/newsletterlistcleanse/Lambda.scala
@@ -13,6 +13,7 @@ import com.gu.newsletterlistcleanse.db.{BigQueryOperations, DatabaseOperations}
 import com.gu.newsletterlistcleanse.models.Newsletter
 import com.gu.newsletterlistcleanse.services.{BrazeUsersService, CleanseListService, CutOffDatesService, NewslettersApiClient, ReportService}
 import org.slf4j.{Logger, LoggerFactory}
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
 
 import scala.beans.BeanProperty
 import scala.concurrent.{Await, Future}
@@ -37,8 +38,10 @@ class Lambda {
 
   val timeout: Duration = Duration(15, TimeUnit.MINUTES)
 
-  val credentialProvider: AWSCredentialsProvider = new NewsletterSQSAWSCredentialProvider()
-  val config: NewsletterConfig = NewsletterConfig.load(credentialProvider)
+  val credentialProvider: AWSCredentialsProvider = NewsletterSQSAWSCredentialProvider.credentialsProviderV1
+  val credentialProviderV2: AwsCredentialsProvider = NewsletterSQSAWSCredentialProvider.credentialsProviderV2
+
+  val config: NewsletterConfig = NewsletterConfig.load(credentialProviderV2)
   val s3Client: AmazonS3 = AmazonS3ClientBuilder.standard
     .withCredentials(credentialProvider)
     .withRegion(Regions.EU_WEST_1)
--- src/main/scala/com/gu/newsletterlistcleanse/NewsletterAWSCredentialProvider.scala
+++ src/main/scala/com/gu/newsletterlistcleanse/NewsletterAWSCredentialProvider.scala
@@ -2,7 +2,18 @@ package com.gu.newsletterlistcleanse
 
 import com.amazonaws.auth.{AWSCredentialsProviderChain, DefaultAWSCredentialsProviderChain}
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import software.amazon.awssdk.auth.credentials.{
+  AwsCredentialsProvider => AwsCredentialsProviderV2,
+  ProfileCredentialsProvider => ProfileCredentialsProviderV2,
+}
 
-class NewsletterSQSAWSCredentialProvider extends AWSCredentialsProviderChain(
-  new ProfileCredentialsProvider("identity"),
-  DefaultAWSCredentialsProviderChain.getInstance())
+object NewsletterSQSAWSCredentialProvider {
+
+  val credentialsProviderV2: AwsCredentialsProviderV2 = ProfileCredentialsProviderV2.create("identity")
+  val credentialsProviderV1: NewsletterSQSAWSCredentialProvider = new NewsletterSQSAWSCredentialProvider()
+
+  class NewsletterSQSAWSCredentialProvider extends AWSCredentialsProviderChain(
+    new ProfileCredentialsProvider("identity"),
+    DefaultAWSCredentialsProviderChain.getInstance())
+
+}
