7914af8f521ce82639b1f6bb442ce05f3c406f1c
Yusuf Faraji <yusuf.faraji@gmail.com>
2021-06-29 17:10:49 +0100

    Add Credentials Provider Chain to fallback to default

--- src/main/scala/com/gu/newsletterlistcleanse/NewsletterAWSCredentialProvider.scala
+++ src/main/scala/com/gu/newsletterlistcleanse/NewsletterAWSCredentialProvider.scala
@@ -3,8 +3,10 @@ package com.gu.newsletterlistcleanse
 import com.amazonaws.auth.{AWSCredentialsProviderChain, DefaultAWSCredentialsProviderChain}
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import software.amazon.awssdk.auth.credentials.{
+  AwsCredentialsProviderChain => AwsCredentialsProviderChainV2,
   AwsCredentialsProvider => AwsCredentialsProviderV2,
-  ProfileCredentialsProvider => ProfileCredentialsProviderV2
+  ProfileCredentialsProvider => ProfileCredentialsProviderV2,
+  DefaultCredentialsProvider => DefaultCredentialsProviderV2
 }
 
 class NewsletterSQSAWSCredentialsProviderV1 extends AWSCredentialsProviderChain(
@@ -12,5 +14,9 @@ class NewsletterSQSAWSCredentialsProviderV1 extends AWSCredentialsProviderChain(
   DefaultAWSCredentialsProviderChain.getInstance())
 
 object NewsletterSQSAwsCredentialsProviderV2 {
-  val credentialsProvider: AwsCredentialsProviderV2 = ProfileCredentialsProviderV2.create("identity")
+  val credentialsProvider: AwsCredentialsProviderV2 = AwsCredentialsProviderChainV2
+    .builder()
+    .addCredentialsProvider(ProfileCredentialsProviderV2.create("identity"))
+    .addCredentialsProvider(DefaultCredentialsProviderV2.create())
+    .build()
 }
